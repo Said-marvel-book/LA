f544818ea3590fe8fa25c3a6eaaf02b4f72207e9
Said-marvel-book <saiderusmanert@gmail.com>
2020-07-18 11:51:23 +0500

    newdocs

--- js/gal.js
+++ js/gal.js
@@ -4,63 +4,67 @@ var imgslide = document.querySelector(".slide");
 var nav = document.querySelector(".nav");
 var tup = document.querySelector(".tup");
 var photos =[
-    {size:[750, 750], text:"Горки - это вещь", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Цветы - очень хороший фон", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Классика. Учитель и ученики.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Футбольная команда и их тренер", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Горы, горы снега. Никогда такого не видел.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Как же хорошо на природе!", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Разделим всё радость, горе и мороженое.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Джунгли над головой", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Китайский храм", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Новый год, новый год. Гирлянда висит", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Сколько народу...", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Мы в пути и сзади экскурсовод", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Мама, Мумтоза, я и сзади статуя привидения", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Ватикан, Ватикан", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Экскурсия в науниках. Вещь ненадёжная. Помню Султан потерялся.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Мама красива и на фоне дождя.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Красивое озеро", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Это был вкусный ужин", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Экскурсия к Коллизею меня разочаровала. Если бы не дождь.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Со стороны мы были похожи на цыганский табор.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Он будет сниться мне в кошмарах.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Я был за рулем", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Мумтоза хотела искупаться в том фонтане.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Мамы", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Саграда Фамилия. Эти бездельники всё не достроят её.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Мама так любит селфи.", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Что это в небе?", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Наш цыганский табор в Диснейленде", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Микки, Минни и мама", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Микки-волшебник", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Брат и мама почему-то корчатся, а я не врубаюсь. Мумтоза", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Крутая машина на нашем фоне", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Как красиво. Но облака всё испортили", transform: "rotate(0deg)"},
-    {size:[750, 750], text:"Вход в Диснейленд.", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Лувр. Огромные залы, статуи и картины. И мама", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"На фотографии можно увидеть  женщину древности и нашу маму. Посудите сами кто красивее. Ответ: Наша мама", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Пирамида Лувра очень хорошо смотрится рядом с Мамой.", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Эйфелева башня в тумане. Очень красиво", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Дом инвалидов. Но их там не было", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Мама, львёнок. Стоп, а где черепаха?", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Это моя семья, но больше половины я не знаю", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Побывать за кулисами-сделано", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Ветер в моих волосах", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Мама села на скамейку. И... Дальше рифму не придумал", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Какие красивые мальчик с мамой. Ну и фонтан.", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Ахмедушка", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Мама и Малика", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Мама на парковке", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Мама на скамейке", transform: "rotate(0deg)"},
-    // {size:[750, 750], text:"Мама у реки", transform: "rotate(0deg)"}
+    {size:[940, 940], text:"Горки - это вещь", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Цветы - очень хороший фон", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Классика. Учитель и ученики.", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Футбольная команда и их тренер", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Горы, горы снега. Никогда такого не видел.", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Как же хорошо на природе!", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Красивая природа", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Такие красивые цветы", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Природа с бассейном", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Красивый вид на город", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Ещё одно групповое селфи", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Горы, горы", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Чёрно-белая фотография Лейлы Алмасовны в подозрительно знакомом месте", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Чёрно-белая фотография Лейлы Алмасовны на подозрительно знакомых качелях", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Очень красивая фотография. Присмотритесь к фону. Я помню этот урок ИЗО.", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Огромный-огромный букет", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Новогодняя фотография двух подруг", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Селфи", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Ещё одно групповое селфи. Я его помню.", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Саидакбар, Лейла Алмасовна и Рухшона", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Лаура Игоревна и Лейла Алмасовна", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Лейла Алмасовна в Самарканде, кажется...", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Лейла Алмасовна и фон водоёма", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Ещё одно красивое селфи", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Селфи и две подруги", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Козим, Жаъфар, Лейла Алмасовна и Рената которая пытается пройти", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Фотография в школе", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Селфи на диване", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Новогоднее селфи и уже три подруги", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"-Привет, солнце, - приветствует солнце Лейла Алмасовна. В ответ молчание.", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Лейла Алмасовна и берёзка", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Лейла Алмасовна и фон из зданий", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Селфи с двумя подругами II", transform: "rotate(0deg)"},
+    {size:[940, 940], text:"Портрет", transform: "rotate(0deg)"},
+    {size:[740, 1040], text:"Жаль, что этот кубок больше не окажется у меня в руках. Но это не про Вас Лейла Алмасовна.", transform: "rotate(0deg)"},
+    {size:[740, 1040], text:"Скрытая съёмка", transform: "rotate(0deg)"},
+    {size:[940, 740], text:"Ещё одно групповое селфи", transform: "rotate(0deg)"},
+    {size:[940, 740], text:"Селфи победителей", transform: "rotate(0deg)"},
+{size:[940, 740], text:"Каждый своё. У Рухшоны шоколад. А у Алихана лего.", transform: "rotate(0deg)"},
+{size:[790, 1090], text:"Поздравляем. Рогатая Рухшона, Саидахмад и Саидакбар.", transform: "rotate(0deg)"},
+{size:[940, 940], text:"Здесь как будто лайки раздавал я. Фотограф точно читает мысли.", transform: "rotate(0deg)"},
+{size:[940, 740], text:"Мы были такие маленькие", transform: "rotate(0deg)"},
+{size:[940, 740], text:"Медалисты и их тренер", transform: "rotate(0deg)"},
+{size:[940, 740], text:"Красивая фотография", transform: "rotate(0deg)"},
+{size:[940, 740], text:"Селфи и без меня всё я обижен до следующей фотографии", transform: "rotate(0deg)"},
+{size:[940, 740], text:"Апельсинчики...ммм. Мы очень вкусно тогда покушали.", transform: "rotate(0deg)"},
+{size:[940, 940], text:"Сумаляк", transform: "rotate(0deg)"},
+{size:[940, 740], text:"А теперь группа фото первоклассников", transform: "rotate(0deg)"},
+{size:[1040, 700], text:"В наших данных ошибка это не первый класс", transform: "rotate(0deg)"},
+{size:[940, 740], text:"А это первый раз в первый класс", transform: "rotate(0deg)"},
+{size:[740, 1040], text:"Скрытая съёмка", transform: "rotate(0deg)"},
+{size:[1040, 700], text:"Зимнее фото", transform: "rotate(0deg)"}
+
+
 ];
 var strr = document.querySelector(".strr");
 strr.addEventListener("click", next);
 var strl = document.querySelector(".strl");
 strl.addEventListener("click", prev);
 function next(){
-    if(counter == 49){
+    if(counter == 51){
         imgslide.setAttribute("src", "img/0.jpg");
         imgslide.style.width = photos[0].size[0] + "px";
         imgslide.style.height = photos[0].size[1] + "px";
@@ -85,14 +89,14 @@ function next(){
 }
 function prev(){
     if(counter == 0){
-        counter = photos.length;
+        counter = 51;
         imgslide.setAttribute("src", "img/" + counter +".jpg");
         imgslide.style.width = photos[counter].size[0] + "px";
         imgslide.style.height = photos[counter].size[1] + "px";
-        tup.textContent = photos[photos.length].text;
-        tup.style.color = photos[photos.length].color;
-        imgslide.style.transform = photos[photos.length].transform;
-        pods.textContent = counter;
+        tup.textContent = photos[51].text;
+        tup.style.color = photos[51].color;
+        imgslide.style.transform = photos[51].transform;
+        pods.textContent = counter+1;
         console.log(counter);
         imgslide.addEventListener("click", bolshe);
     }else{
